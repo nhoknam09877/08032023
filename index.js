6e7e5f6aaf079eba4fb626960fec15042189353b
nhoknam09877 <namtqth2210018@fpt.edu.vn>
2023-03-16 15:20:50 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let search= document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = ()=>{
+    search.classList.toggle('activex');
+}
+// slide
+var slides= document.querySelectorAll('.slider');
+var btns= document.querySelectorAll('.btn');
+let currentSlide= 1;
+
+var manualNav = function(manual){
+    slides.forEach((slider)=>{
+        slider.classList.remove('active');
+
+        btns.forEach((btn)=>{
+            btn.classList.remove('active');
+        });
+
+
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        manualNav(i);
+        currentSlide=i;
+
+    });
+});
+
+
+
+
+// autoplay
+var repeat = function(activeClass){
+    let active =document.getElementsByClassName('active');
+    let i=1;
+
+    var repeater =() =>{
+        setTimeout(function(){
+            [...active].forEach((activeSlide)=>{
+                activeSlide.classList.remove('active');
+            });
+
+    
+            slides[i].classList.add('active')
+            btns[i].classList.add('active')
+            i++;
+
+            if(slides.length ==i){
+                i=0
+            }
+            if(i>=slides.length){
+                return;
+            }
+            repeater();
+
+        },10000);
+    }
+    repeater();
+}
+repeat();
