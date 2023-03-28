9c015556c1627b29596804c6972b1909d9ce8af7
nhoknam09877 <namtqth2210018@fpt.edu.vn>
2023-03-28 15:24:29 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let search= document.querySelector('.search-box');
-document.querySelector('#search-icon').onclick = ()=>{
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('activex');
 }
 // slide
-var slides= document.querySelectorAll('.slider');
-var btns= document.querySelectorAll('.btn');
-let currentSlide= 1;
+var slides = document.querySelectorAll('.slider');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
 
-var manualNav = function(manual){
-    slides.forEach((slider)=>{
+var manualNav = function (manual) {
+    slides.forEach((slider) => {
+
         slider.classList.remove('active');
-
-        btns.forEach((btn)=>{
+        btns.forEach((btn) => {
             btn.classList.remove('active');
         });
 
@@ -23,10 +23,10 @@ var manualNav = function(manual){
 }
 
 
-btns.forEach((btn,i)=>{
-    btn.addEventListener("click",()=>{
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
         manualNav(i);
-        currentSlide=i;
+        currentSlide = i;
 
     });
 });
@@ -35,31 +35,48 @@ btns.forEach((btn,i)=>{
 
 
 // autoplay
-var repeat = function(activeClass){
-    let active =document.getElementsByClassName('active');
-    let i=1;
+var repeat = function (activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i = 1;
 
-    var repeater =() =>{
-        setTimeout(function(){
-            [...active].forEach((activeSlide)=>{
+    var repeater = () => {
+        setTimeout(function () {
+            [...active].forEach((activeSlide) => {
                 activeSlide.classList.remove('active');
             });
 
-    
+
             slides[i].classList.add('active')
             btns[i].classList.add('active')
             i++;
 
-            if(slides.length ==i){
-                i=0
+            if (slides.length == i) {
+                i = 0
             }
-            if(i>=slides.length){
+            if (i >= slides.length) {
                 return;
             }
             repeater();
 
-        },10000);
+        }, 10000);
     }
     repeater();
 }
 repeat();
+
+// đánh giá
+$(document).ready(function () {
+    $('.testi-content').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='bx bx-left-arrow-alt' ></i></button>",
+        nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='bx bx-right-arrow-alt'></i></button>",
+        
+    });
+});
+
+
+// đăng kí nhận tên
+function validation( ){
+    alert("working");
+}
---
+++ index3/index3.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        
+      });
+
+              
+});
